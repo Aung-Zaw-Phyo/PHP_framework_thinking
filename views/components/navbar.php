8ecb003c18747dc42ab3d0b00d1cfadb266042ff
Aung Zaw Phyo <aungzawphyo1102@gmail.com>
2022-07-29 11:41:27 +0630

    DRY view files

---
+++ views/components/navbar.php
@@ -0,0 +1,8 @@
+<div class="container">
+        <div class="d-flex justify-content-center">
+            <a href="/" class="text-decoration-none m-3">Home</a>
+            <a href="about" class="text-decoration-none m-3">About</a>
+            <a href="contact" class="text-decoration-none m-3">Contact</a>
+            <a href="product" class="text-decoration-none m-3">Product</a>
+        </div>
+</div>
